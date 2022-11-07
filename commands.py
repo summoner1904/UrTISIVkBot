c962f92f6a99ce8b75defd914df790d9ddadad8a
Азимов Тимур <115421143+summoner1904@users.noreply.github.com>
2022-11-07 23:02:53 +0500

    Добавлена конструкция if __name__ == "__main__"

--- commands.py
+++ commands.py
@@ -1,15 +1,16 @@
 from server import Server
-bot = Server()
 
-command_list = {
-    "привет": bot.command_hi,
-    "понедельник": bot.command_monday,
-    "вторник": bot.command_tuesday,
-    "среда": bot.command_wednesday,
-    "четверг": bot.command_thursday,
-    "пятница": bot.command_friday,
-    "суббота": bot.command_saturday, 
-    "клавиатура": bot.command_keyboard,
-    "ошибка": bot.command_error
-}
-bot.start(command_list)
+if __name__ == "__main__":
+    bot = Server()
+    COMMAND_LIST = {
+        "привет": bot.command_hi,
+        "понедельник": bot.command_monday,
+        "вторник": bot.command_tuesday,
+        "среда": bot.command_wednesday,
+        "четверг": bot.command_thursday,
+        "пятница": bot.command_friday,
+        "суббота": bot.command_saturday, 
+        "клавиатура": bot.command_keyboard,
+        "ошибка": bot.command_error
+    }
+    bot.start(COMMAND_LIST)
