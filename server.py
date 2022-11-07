f74e79a2aae37096a52a1e52ca614a774e371690
Азимов Тимур <115421143+summoner1904@users.noreply.github.com>
2022-11-07 23:00:55 +0500

    Написана документация
    Теперь клавиатура возвращается любой командой.

--- server.py
+++ server.py
@@ -1,17 +1,26 @@
 import random
 import vk_api
-from base import *
+from database import *
 from vk_api.longpoll import VkEventType, VkLongPoll
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from config import TOKEN
+from abstract.abstract_server import AbstractBaseServer, AbsctractKeyboardMixin
 
 
-class BaseServer:
+class BaseServer(AbstractBaseServer):
+    """
+    Класс, отвечающий за авторизацию в лице сообщества ВК.
+    """
     vk = vk_api.VkApi(token=TOKEN)
     longpoll = VkLongPoll(vk)
 
-    def start(self, command_list: dict):
-        self.commands = command_list
+    def start(self, COMMAND_LIST: dict):
+        """
+        Работает с классом Today - получает актуальную дату. Инициализирует command_list.
+        :param command_list (Словарь с командами)
+        :return: None
+        """
+        self.commands = COMMAND_LIST
         for event in self.longpoll.listen():
             Today.today = datetime.datetime.now()
             Today.today_str = Today.today.strftime("%d-%m-%y")
@@ -21,16 +30,31 @@ class BaseServer:
             self.command_worker(event)
     
     def command_worker(self, event):
-        if event.type == VkEventType.MESSAGE_NEW:
-            if event.to_me:
-                if self.commands.get(event.text.lower()):
-                    self.commands[event.text.lower()](event)
-                else:
-                    self.commands["ошибка"](event)
+        """
+        Обрабатывает сообщения пользователя. Если находит сообщение пользоваля в command_list, то выполняет команду.
+        Если сообщения пользователя нет в command_list, то обращается к ключу "ошибка" в словаре command_list.
+        :param event
+        :return: None
+        """
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            if self.commands.get(event.text.lower()):
+                self.commands[event.text.lower()](event)
+            else:
+                self.commands["ошибка"](event)
 
 
 class UtilsServer(BaseServer):
+    """
+    Класс, отвечающий за отправку сообщения пользователю от лица сообщества.
+    """
     def send_msg(self, user_id, message, keyboard = None):
+        """
+        Отправляет сообщение пользователю.
+        :param user_id (id пользователя, кому отправить сообщение)
+        :param message (сообщение, которое нужно отправить пользователю)
+        :param keyboard 
+        :return: None
+        """
         if keyboard:
             params = {
                 "user_id": user_id,
@@ -48,49 +72,92 @@ class UtilsServer(BaseServer):
 
 
 class Server(UtilsServer):
+    """
+    Класс с командами.    
+    """
     def __init__(self):
+        """
+        Инициализация клавиатуры
+        :return: None
+        """
         self.keyboard = KeyboardMixin()
         
     def command_error(self, event):
+        """
+        Команда, подсказывающая пользователю о том, что необходимо написать/выбрать.
+        :param event
+        :return: Возвращает метод send_msg с сообщением "Введите или выберите день недели!".
+        """
         self.send_msg(
             event.user_id,
-            f"Введите или выберите день недели!",
+            "Введите или выберите день недели!",
             keyboard=self.keyboard.get_standart_keyboard()
         )
 
     def command_hi(self, event):
+        """
+        Команда, передающая сообщение "Привет!" в метод send_msg. 
+        :param event
+        :return: Возвращает метод send_msg с сообщением "Привет!".
+        """
         self.send_msg(
             event.user_id,
-            f"Привет!"
+            "Привет!",
+            keyboard=self.keyboard.get_standart_keyboard()
         )
     def command_keyboard(self, event):
+        """
+        Команда, отвечающая за вывод клавиатуры в боте.
+        :param event
+        :return: Возвращает клавиатуру и сообщение "Держи клавиатуру!" (str).
+        """
         self.send_msg(
             event.user_id,
-            f"Держи клавиатуру!",
+            "Держи клавиатуру!",
             keyboard = self.keyboard.get_standart_keyboard()
         )
     def command_monday(self, event):
+        """
+        Команда, формирующая расписание на понедельник.
+        :param event
+        :return: Возвращает метод send_msg с расписанием на понедельник (list) и стандартную клавиатуру.
+        """
         self.send_msg(
             event.user_id,
-            "\n".join(mondaay),
+            "\n".join(monday_upd),
             keyboard=self.keyboard.get_standart_keyboard()
         )
     
     def command_tuesday(self, event):
+        """
+        Команда, формирующая расписание на вторник.
+        :param event
+        :return: Возвращает метод send_msg с расписанием на вторник (list) и стандартную клавиатуру.
+        """
         self.send_msg(
             event.user_id,
-            "\n".join(tuesdaay),
+            "\n".join(tuesday_upd),
             keyboard=self.keyboard.get_standart_keyboard()
         )
     
     def command_wednesday(self, event):
+        """
+        Команда, формирующая расписание на среду.
+        :param event
+        :return: Возвращает метод send_msg с расписанием на среду (list) и стандартную клавиатуру.
+        """
         self.send_msg(
             event.user_id,
-            "\n".join(wednesdaay),
+            "\n".join(wednesday_upd),
             keyboard=self.keyboard.get_standart_keyboard()
         )
     
     def command_thursday(self, event):
+        """
+        Команда, формирующая расписание на четверг.
+        :param event
+        :return: Возвращает метод send_msg с расписанием на четверг (list) и стандартную клавиатуру.
+        """ 
         self.send_msg(
             event.user_id,
             "\n".join(thursday),
@@ -98,13 +165,23 @@ class Server(UtilsServer):
         )
     
     def command_friday(self, event):
+        """
+        Команда, формирующая расписание на пятницу.
+        :param event
+        :return: Возвращает метод send_msg с расписанием на пятницу (list) и стандартную клавиатуру.
+        """
         self.send_msg(
             event.user_id,
-            "\n".join(fridaay),
+            "\n".join(friday_upd),
             keyboard=self.keyboard.get_standart_keyboard()
         )
     
     def command_saturday(self, event):
+        """
+        Команда, формирующая расписание на субботу.
+        :param event
+        :return: Возвращает метод send_msg с расписанием на субботу (list) и стандартную клавиатуру.
+        """
         self.send_msg(
             event.user_id,
             "\n".join(saturday),
@@ -112,8 +189,16 @@ class Server(UtilsServer):
         )
 
 
-class KeyboardMixin(VkKeyboard):
+class KeyboardMixin(VkKeyboard, AbsctractKeyboardMixin):
+    """
+    Класс, отвечающий за клавиатуру. 
+    :return: keyboard
+    """
     def get_standart_keyboard(self):
+        """
+        Стандартная клавиатура (со всеми днями недели).
+        :return: Возвращает стандартную клавиатуру. 
+        """
         keyboard = VkKeyboard()
         keyboard.add_button(label='Понедельник', color=VkKeyboardColor.PRIMARY)
         keyboard.add_button(label='Вторник', color=VkKeyboardColor.PRIMARY)
